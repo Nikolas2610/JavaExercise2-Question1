b10d59faab14cef9758b5e034671783da04c307c
Nikolas2610 <npsillou@gmail.com>
2023-02-06 19:19:59 +0200

    Update the data structure for the graph

--- src/EdgeServer.java
+++ src/EdgeServer.java
@@ -7,6 +7,9 @@ import java.net.Socket;
 
 public class EdgeServer {
     public static void main(String[] args) throws IOException {
+//      Get the random Graph
+        Graph graph = new Graph(3000000, 1000000);
+
 //      Initialize socket server with port
         ServerSocket serverSocket = null;
         final int PORT = 10007;
@@ -37,57 +40,60 @@ public class EdgeServer {
         BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         String inputLine;
 
-//      Get the random Graph
-        EdgeList edgeList = new EdgeList();
-
         while (true) {
 //          Server send message to client for the details tha wants
-            out.println("Write to keyboard the edge. Nodes are " + edgeList.getNODES() +
-                    " and the accept format is X,X with max value " + (edgeList.getNODES() - 1)
+            out.println("Write to keyboard the edge. Nodes are " + graph.getNODES() +
+                    " and the accept format is X,X with max value " + (graph.getNODES() - 1)
                     + " (example: 4,2)\nFor exit write -1,-1:");
 //          Server waiting for response from client
             inputLine = in.readLine();
+
+            if (inputLine.equals("save-file")) {
+//          Save the graph to the graph.txt file
+                graph.saveGraphToFile();
+            } else {
 //          Checking the empty message
-            if (inputLine == null || inputLine.length() == 0) {
-                out.println("Empty input");
-                continue;
-            }
+                if (inputLine == null || inputLine.length() == 0) {
+                    out.println("Empty input");
+                    continue;
+                }
 //          Split the message from client
-            String[] edges = inputLine.split(",");
+                String[] edges = inputLine.split(",");
 //          Checking if the array is the correct
-            if (edges.length == 2) {
-                try {
+                if (edges.length == 2) {
+                    try {
 //                  Transform the string data to numbers
-                    int[] edgesInt = new int[2];
-                    for (int i = 0; i < edges.length; i++) {
-                        edgesInt[i] = Integer.parseInt(edges[i]);
-                    }
+                        int[] edgesInt = new int[2];
+                        for (int i = 0; i < edges.length; i++) {
+                            edgesInt[i] = Integer.parseInt(edges[i]);
+                        }
 //                  Check if the user want to close the connection
-                    if (edges[0].equals("-1") && edges[1].equals("-1")) {
-                        System.out.println("Close Server!");
-                        out.println("Close Server!");
-                        break;
-                    }
+                        if (edges[0].equals("-1") && edges[1].equals("-1")) {
+                            System.out.println("Close Server!");
+                            out.println("Close Server!");
+                            break;
+                        }
 //                  Check if the user gives available numbers with the min and max nodes
-                    if (edgesInt[0] >= edgeList.getNODES() || edgesInt[1] >= edgeList.getNODES() ||
-                    edgesInt[0] < 0 || edgesInt[1] < 0) {
+                        if (edgesInt[0] >= graph.getNODES() || edgesInt[1] >= graph.getNODES() ||
+                                edgesInt[0] < 0 || edgesInt[1] < 0) {
+                            out.println("Wrong input");
+                            continue;
+                        }
+//                  Check if the edge exists to the graph and create message for the client
+                        String message = graph.edgeExists(edgesInt[0], edgesInt[1]);
+                        System.out.println("Row: " + edges[0] + " Col: " + edges[1]);
+//                  Send the server response to the client
+                        out.println(message);
+                    } catch (NumberFormatException e) {
+                        // Handle the exception if the user input are numbers
                         out.println("Wrong input");
                         continue;
                     }
-//                  Check if the edge exists to the graph and create message for the client
-                    String message = edgeList.edgeExists(edgesInt[0], edgesInt[1]);
-                    System.out.println("Row: " + edges[0] + " Col: " + edges[1]);
-//                  Send the server response to the client
-                    out.println(message);
-                } catch (NumberFormatException e) {
-                    // Handle the exception if the user input are numbers
+                } else {
+//              Capture the error for bad input
                     out.println("Wrong input");
                     continue;
                 }
-            } else {
-//              Capture the error for bad input
-                out.println("Wrong input");
-                continue;
             }
         }
 //      Close connection
